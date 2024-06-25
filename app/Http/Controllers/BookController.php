b351c311964cd8635ed792621111a852ab8cc3fa
Marco Caggiano <marco.caggiano@finconsgroup.com>
2024-06-26 00:03:46 +0200

    Added model book + controller

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -2,47 +2,58 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Book;
 use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+    // Display a listing of books
     public function index()
     {
-        //
+        return response()->json(Book::all(), 200);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
+    // Store a newly created book
     public function store(Request $request)
     {
-        //
+        $validated = $request->validate([
+            'name' => 'required|string',
+            'description' => 'required|string',
+            'cover' => 'required|image',
+            'price' => 'required|integer',
+        ]);
+
+        $book = Book::create($validated);
+
+        return response()->json($book, 201);
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
+    // Display the specified book
+    public function show(Book $book)
     {
-        //
+        return response()->json($book, 200);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
+    // Update the specified book
+    public function update(Request $request, Book $book)
     {
-        //
+        $validated = $request->validate([
+            'name' => 'required|string',
+            'description' => 'required|string',
+            'cover' => 'required|image',
+            'price' => 'required|integer',
+        ]);
+
+        $book->update($validated);
+
+        return response()->json($book, 200);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
+    // Remove the specified book
+    public function destroy(Book $book)
     {
-        //
+        $book->delete();
+
+        return response()->json(null, 204);
     }
 }
